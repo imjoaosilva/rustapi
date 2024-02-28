b5f4437b2b529d6ce24e0ac55cdd2ce66a4e1004
=Z4 <z4kreyyt@gmail.com>
2024-02-28 23:28:52 +0000

    docs

--- src/routers/authentication.rs
+++ src/routers/authentication.rs
@@ -1,7 +1,19 @@
 
 use axum::{routing::get,routing::post, Router};
 use super::{login,register};
-    
+
+
+/// Creates and returns a router for authentication endpoints.
+///
+/// # Examples
+///
+/// ```
+/// use rustapi::routers::authentication::router;
+///
+/// let auth_router = router();
+/// // Use the `auth_router` to handle authentication routes
+/// ```
+
 pub fn router() -> Router {
     Router::new()
         .route("/login", post(login))
